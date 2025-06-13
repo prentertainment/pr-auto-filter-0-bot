17ca126e3a7df64bf88d27e4ba8e56275dabe6be
prentertainment <prentertainment1st@gmail.com>
2025-06-14 05:05:21 +0600

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,9 @@
+async def get_size(size):
+    """Convert file size to human readable format"""
+    units = ["Bytes", "KB", "MB", "GB", "TB"]
+    size = float(size)
+    i = 0
+    while size >= 1024 and i < len(units)-1:
+        size /= 1024
+        i += 1
+    return f"{size:.2f} {units[i]}"
